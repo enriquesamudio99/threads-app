c502412bc6535322ab41f89828e826aa9383d405
Enrique <enriquesamudio1999@gmail.com>
2023-11-17 15:17:05 -0300

    First commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from 'next'
+import { ClerkProvider } from '@clerk/nextjs'
+import { Inter } from 'next/font/google'
+import '../globals.css'
+import TopBar from '@/components/shared/TopBar'
+import LeftSidebar from '@/components/shared/LeftSidebar'
+import RightSidebar from '@/components/shared/RightSidebar'
+import BottomBar from '@/components/shared/BottomBar'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Threads',
+  description: 'A Next.js 14 Threads Application'
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <TopBar /> 
+          <main className="flex flex-row">
+            <LeftSidebar />
+            <section className="main-container">
+              <div className="w-full max-w-4xl">
+                {children}
+              </div>
+            </section>
+            <RightSidebar />
+          </main>
+          <BottomBar />
+        </body>
+      </html>
+    </ClerkProvider>
+  )
+}
